b1212c7b1ffb6753e471ea93b7cf04da73d27563
rbonapaz-Bonapaz <rbonapaz@gmail.com>
2026-07-22 17:31:31 -0300

    Add files via upload

---
+++ Fabi-TO-Gestao/config.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Configuração oficial do Firebase.
+ */
+export const firebaseConfig = {
+  apiKey: "AIzaSyA66pkOGy5NCTXnrZq5F8d6h7mB5lXT1mc",
+  authDomain: "studio-8622003232-9c24d.firebaseapp.com",
+  projectId: "studio-8622003232-9c24d",
+  storageBucket: "studio-8622003232-9c24d.firebasestorage.app",
+  messagingSenderId: "8622003232",
+  appId: "1:607989208535:web:e9e6db0d1867dbcc21e792",
+};
