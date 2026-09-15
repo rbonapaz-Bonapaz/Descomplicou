46316d323216df6723c8d888aa739369ebed81f4
rbonapaz-Bonapaz <rbonapaz@gmail.com>
2026-09-15 09:15:10 -0300

    Delete Fabi-TO-Gestao directory

--- Fabi-TO-Gestao/src/firebase/index.ts
+++
@@ -1,9 +0,0 @@
-
-'use client';
-
-export * from './app';
-export * from './provider';
-export * from './auth/use-user';
-export * from './firestore/use-collection';
-export * from './firestore/use-doc';
-export { FirebaseClientProvider } from './client-provider';
